a16df9051cb67a09330031df9d2a5e4f682f0b76
sqirum <pauloslh94@gmail.com>
2022-10-02 23:37:14 +0100

    fix typo err

--- app.js
+++ app.js
@@ -10,14 +10,46 @@ const updatePokemon = async (name) => {
     return pokemon;
 }
 
-request.addEventListener('submit', (e) => {
+const updateUI = (data) => {
+    // card.innerHTML = `
+    //     <h3 class="text-center text-uppercase">${data.name}</h3>
+    //     <ul class="mt-3">
+    //     <li>Habilidade 1: ${data.abilities[0].ability.name}</li>
+    //     <li>Habilidade 2: ${data.abilities[1].ability.name}</li>
+    //     </ul>
+    //     <div class="d-flex justify-content-evenly mt-3">
+    //     <p class="bg-danger rounded-pill  px-4 py-3 text-white">${data.types[0].type.name}</p>
+    //     <p class="bg-info rounded-pill px-4 py-3 text-white"> ${data.types[1].type.name}</p>
+    //     </div>
+    //     `;
+    card.innerHTML = `
+        <h3 class="text-center text-uppercase">${data.name}</h3>
+        <ul class="mt-3">
+        <li>Habilidade 1: ${data.abilities[0].ability.name}</li>
+        </ul>
+        <div class="d-flex justify-content-evenly mt-3">
+        <p class="bg-danger rounded-pill  px-4 py-3 text-white">${data.types[0].type.name}</p>
+        </div>
+        `;
+
+    console.log(data);
+}
+
+request.addEventListener('click', (e) => {
 
     e.preventDefault();
 
     updatePokemon(input.value)
-        .then(data => console.log(data))
+        .then(data => updateUI(data))
         .catch(err => console.log(err));
+
+    localStorage.setItem('pokemon', input.value);
 
     search.reset();
 });
 
+if (localStorage.getItem('pokemon')) {
+    updatePokemon( (localStorage.getItem('pokemon')) )
+        .then(data => updateUI(data))
+        .catch( err => console.log(err));
+}
